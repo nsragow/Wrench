235562059e322f64989e26b1a7d2b78cdb81536e
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-07-13 16:31:04 -0400

    Added begining pieces to map creator

---
+++ Assets/SpriteScaler/SpriteScaler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScaler : MonoBehaviour
+{
+    public int countPerScreen;
+    public new SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        double verticalSize = Camera.main.orthographicSize * 2.0;
+        double aspect = Camera.main.aspect;
+        print(aspect);
+        print("aspect");
+        print(Screen.width / Screen.height);
+        print("what");
+        double horizontalSize = (aspect)*verticalSize;
+        print(Screen.width);
+        print(Screen.height);
+        Vector2 spriteSize = renderer.bounds.size;
+        print("hor" +horizontalSize);
+        print("ver" + verticalSize);
+
+        float width = spriteSize.x;
+        print(width);
+        print(renderer.size.x);
+        print(transform.localScale.x);
+
+        float newScale =(float) horizontalSize / (width*countPerScreen);
+        print(newScale);
+        float x = newScale;
+        float y = newScale;
+        float z = transform.localScale.z;
+
+
+        transform.localScale = new Vector3(x, y, z);
+    }
+
+    
+}
