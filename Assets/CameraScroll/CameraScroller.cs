eb2ac1f9eb6f02a6f99fb6abf800ece974ee54ab
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-07-13 12:16:15 -0400

    Camera scroller

---
+++ Assets/CameraScroll/CameraScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Intended to be attached to the camera
+/// Controls the scrolling of the camera
+/// </summary>
+public class CameraScroller : MonoBehaviour
+{
+    public float initialSpeed;
+    Rigidbody2D cameraRigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRigid = gameObject.AddComponent<Rigidbody2D>();
+        cameraRigid.gravityScale = 0;
+        cameraRigid.AddForce(new Vector2(0, initialSpeed));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
