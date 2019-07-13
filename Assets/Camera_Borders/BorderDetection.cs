6a5fe937fcf8694e96c74b757d3ce2414e1268c0
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-07-13 12:00:14 -0400

    Added BorderDection system:

---
+++ Assets/Camera_Borders/BorderDetection.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderDetection : MonoBehaviour
+{
+    /// <summary>
+    /// Any objects placed in here from the editor will be 'Track'ed
+    /// </summary>
+    public GameObject[] trackThese;
+
+    /// <summary>
+    /// Attaches a tracker to the GameObject passed.
+    /// will call ImOutOfView when that GameObject is
+    /// out of view.
+    /// </summary>
+    /// <param name="toTrack">untracked GameObject to be tracked</param>
+    public void Track(GameObject toTrack)
+    {
+        FellOut newFellOut = toTrack.AddComponent<FellOut>();
+        newFellOut.SetBorderDetector(this);
+    }
+
+    /// <summary>
+    /// Do not call this - Reserved for tracked Gameobjects
+    /// </summary>
+    /// <param name="trackedObject">tracked gameobject that fell out of view</param>
+    public void ImOutOfView(GameObject trackedObject)
+    {
+        switch (GetGameType(trackedObject))
+        {
+            case "wrench":
+                WrenchDied(trackedObject);
+                break;
+            default:
+                throw new System.Exception("Not Implemented");
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject go in trackThese)
+        {
+            this.Track(go);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private string GetGameType(GameObject toCheck)
+    {
+        //todo, finish this function
+        return "wrench";
+    }
+    private void WrenchDied(GameObject wrench)
+    {
+        Debug.LogWarning("Not Implemented WrenchDied in BorderDetection");
+
+    }
+}
---
+++ Assets/Camera_Borders/FellOut.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FellOut : MonoBehaviour
+{
+    private BorderDetection detector;
+    // Disable the behaviour when it becomes invisible...
+    void OnBecameInvisible()
+    {
+        detector.ImOutOfView(gameObject);
+    }
+    /// <summary>
+    /// Reserved for BorderDetector;
+    /// call when initializing FellOut
+    /// </summary>
+    /// <param name="detector">the detector to ping on out of view</param>
+    public void SetBorderDetector(BorderDetection detector)
+    {
+        print("called");
+        if(this.detector != null)
+        {
+            throw new System.Exception("Tried to set the border detector when it was already set");
+        }
+        this.detector = detector;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
