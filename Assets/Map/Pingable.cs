90cada99d573e09f4d50d4f823be714f519c4ad4
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-07-13 20:43:35 -0400

    Classes that were intended to be part of the pooling scheme but due to lack of time were severly torn down after unexplained bugs were crashing the system. They still exist due to their coupling with working systems

---
+++ Assets/Map/Pingable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Originally part of the grander pool scheme which failed.
+/// 
+/// Simple class that runs a Callable
+/// objects call method when this is pinged
+/// </summary>
+public class Pingable : MonoBehaviour
+{
+    Callable onPingAction;
+    /// <summary>
+    /// Set the Callable object to be the object called
+    /// When this is pinged
+    /// </summary>
+    /// <param name="callable">To be called on ping</param>
+    public void OnPing(Callable callable)
+    {
+        onPingAction = callable;
+    }
+    /// <summary>
+    /// Exposed method: call this to run
+    /// the Callables Call method
+    /// </summary>
+    /// <param name="from"></param>
+    public void Ping(GameObject from)
+    {
+        //print("finally!");
+
+        onPingAction.Call();
+    }
+}
